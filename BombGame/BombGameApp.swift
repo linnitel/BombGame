e7696493ff821eaa34dee38caeb6dc6a78cc02cc
Julia Martsenko <jm@letmespeak.org>
2025-02-10 17:27:25 +0100

    Initial Commit

---
+++ BombGame/BombGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BombGameApp.swift
+//  BombGame
+//
+//  Created by Julia Martcenko on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BombGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
