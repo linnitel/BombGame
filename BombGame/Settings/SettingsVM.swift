7ed6ac901a2c5d566e1d504afad6a51396186cc0
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-02-14 19:23:55 +0300

    //

--- BombGame/Settings/SettingsVM.swift
+++ BombGame/Settings/SettingsVM.swift
@@ -13,14 +13,14 @@ class SettingsVM: ObservableObject {
     let bombSounds = ["Часы1", "Часы2", "Часы3"]
     let explosionSounds = ["Взрыв1", "Взрыв2", "Взрыв3"]
     
-    @Published var backgroundMusic = "Мелодия1" { didSet{print(backgroundMusic)}}
+    @Published var backgroundMusic = "Мелодия1" { didSet{print(gameTime)}}
     @Published var bombSound = "Часы1"
     @Published var explosionSound = "Взрыв1"
 
     @Published var vibration = true 
-    @Published var gameForTasks = false
+    @Published var gameForTasks = true
     
-    var gameTime = 30 // 10 - 30 - 60 - random(20...120)seconds
+    var gameTime = 30   // { didSet{print(gameTime)}} // 10 - 30 - 60 - random(20...120)seconds
     
     func setGameTime(time: Int) {
         gameTime = time
