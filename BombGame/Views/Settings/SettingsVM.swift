0d6086eb25722812504caacaf45aab52cb5fdaca
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-02-16 11:59:36 +0300

    add Settings

---
+++ BombGame/Views/Settings/SettingsVM.swift
@@ -0,0 +1,55 @@
+//
+//  SettingsVM.swift
+//  BombGame
+//
+//  Created by Marat Fakhrizhanov on 14.02.2025.
+//
+
+import SwiftUI
+
+class SettingsVM: ObservableObject {
+    
+    let backgroundSounds = ["Мелодия1", "Мелодия2", "Мелодия3"]
+    let bombSounds = ["Часы1", "Часы2", "Часы3"]
+    let explosionSounds = ["Взрыв1", "Взрыв2", "Взрыв3"]
+    
+    // AppStorage("") test
+    @Published var backgroundMusic: String = (UserDefaults.standard.string(forKey: "backgroundMusic") ?? "Мелодия1")
+    @Published var bombSound: String = (UserDefaults.standard.string(forKey: "bombSound") ?? "Взрыв1")
+    @Published var explosionSound: String = (UserDefaults.standard.string(forKey: "explosionSound") ?? "Часы1")
+    
+    
+    @Published var vibration = UserDefaults.standard.bool(forKey: "vibration") {
+        didSet {
+            UserDefaults.standard.set(vibration, forKey: "vibration")
+        }
+    }
+    @Published var gameForTasks = UserDefaults.standard.bool(forKey: "gameForTasks") {
+        didSet {
+            UserDefaults.standard.set(gameForTasks, forKey: "gameForTasks")
+        }
+    }
+    
+    var gameTime = UserDefaults.standard.value(forKey: "gameTime") ?? 20
+    
+    //MARK: - Settings func
+    func setGameTime(time: Int) {
+        gameTime = time
+        UserDefaults.standard.set(time, forKey: "gameTime")
+    }
+    
+    func setBackgroundMusic(music: String) {
+        UserDefaults.standard.set(music, forKey: "backgroundMusic")
+    }
+    
+    func setBombSound(sound: String) {
+        UserDefaults.standard.set(sound, forKey: "bombSound")
+    }
+    
+    func setExplosionSound(sound: String) {
+        UserDefaults.standard.set(sound, forKey: "explosionSound")
+    }
+    
+    
+}
+
---
+++ BombGame/Views/Settings/TimeButtons.swift
@@ -0,0 +1,62 @@
+//
+//  TimeButttons.swift
+//  BombGame
+//
+//  Created by Marat Fakhrizhanov on 14.02.2025.
+//
+
+import SwiftUI
+
+struct TimeButtons: View {
+    
+    let textOne: String
+    let textTwo: String
+    
+    let funcOne: () -> Void
+    let funcTwo: () -> Void
+    
+    var body: some View {
+        HStack(spacing: 44) {
+            Button {
+                if SettingsVM().vibration {
+                    let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+                        impactHeavy.impactOccurred()
+                    print("vibro time")
+                }
+                funcOne()
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundStyle(.appPrimary)
+                        .frame(width: 134, height: 40)
+                    Text(textOne)
+                        .font(.system(size: 16, weight: .bold, design: .rounded))
+                        .foregroundStyle(.white)
+                }
+            }
+            
+            Button{
+                if SettingsVM().vibration {
+                    let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+                        impactHeavy.impactOccurred()
+                    print("vibro time")
+                }
+                
+                funcTwo()
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 134, height: 40)
+                    Text(textTwo)
+                        .font(.system(size: 16, weight: .bold, design: .rounded))
+                        .foregroundStyle(.white)
+                }
+            }
+        }
+        .padding(.horizontal,20)
+    }
+}
+
+#Preview {
+    TimeButtons(textOne: "Коротkое", textTwo: "Среднее", funcOne: {}, funcTwo: {})
+}
